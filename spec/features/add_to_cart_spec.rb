5444cbbe1772c2c801286fc0c429ec616e0f6cc3
Ken Cruz <ken@kencruz.ca>
2022-01-12 15:47:22 -0500

    implemented add to cart feature spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Cart count increment by 1" do
+
+    visit root_path
+
+    first('.product').click_on('Add')
+
+    save_screenshot
+
+    expect(page).to have_text 'My Cart (1)'
+  end
+end
