5dd393643598f50ad66672e35faea2aae4d4623a
Ken Cruz <ken@kencruz.ca>
2022-01-12 15:33:26 -0500

    implemented product details feature spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    click_link 'Details'
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail', count: 1
+    save_screenshot
+  end
+
+end
