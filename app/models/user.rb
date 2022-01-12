54f06598dbd94cd6e60ce819e7660e22f8fa8609
Ken Cruz <ken@kencruz.ca>
2022-01-11 19:46:17 -0500

    added User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8}
+  validates_confirmation_of :password
 end
