6ca8b687a313d7a58f9b82e0b184dda66be63fb7
Ken Cruz <ken@kencruz.ca>
2022-01-11 21:28:22 -0500

    User model tests for name, email validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,6 +51,36 @@ RSpec.describe User, type: :model do
 
       expect(@user2).to_not be_valid
     end
+
+    it "is not valid without an email" do
+      @user = User.new
+      @user.first_name = "Ken"
+      @user.last_name = "Masters"
+      @user.password = "12341234"
+      @user.password_confirmation = "12341234"
+
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a first name" do
+      @user = User.new
+      @user.last_name = "Masters"
+      @user.email = "test@example.com"
+      @user.password = "12341234"
+      @user.password_confirmation = "12341234"
+
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      @user = User.new
+      @user.first_name = "Ken"
+      @user.email = "test@example.com"
+      @user.password = "12341234"
+      @user.password_confirmation = "12341234"
+
+      expect(@user).to_not be_valid
+    end
   end
 
 end
